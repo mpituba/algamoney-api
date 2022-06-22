46c28c51387eab0cb7c62c51ff3b485199b2eefd
Marcelo Pituba <mpituba@yahoo.com>
2022-06-22 13:45:39 -0300

    6.5.Configurando JWT no projeto

--- algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -1,6 +1,13 @@
 package com.example.algamoney.api.config;
 
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,8 +16,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -33,24 +46,60 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			//Permite acesso a categorias	
 			.antMatchers("/categorias").permitAll()
 			.anyRequest().authenticated()
-			.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
-		.csrf().disable()
-		.oauth2ResourceServer().opaqueToken();
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+			.csrf().disable()
+			.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+	}
+	
+	 @Bean
+	    public JwtDecoder jwtDecoder() {
+		 	//Precisar ser uma chave de 32bits
+	        var secretKey = new SecretKeySpec("3032885ba9cd6621bcc4e7d6b6c35c2b".getBytes(), "HmacSHA256");
+
+	        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+	    }
+	 
+
+		@Bean
+		@Override
+		protected AuthenticationManager authenticationManager() throws Exception {
+			return super.authenticationManager();
+		}
+		
+		
+		@Bean
+		public PasswordEncoder paswordEncoder() {
+			return NoOpPasswordEncoder.getInstance();
+		}
+	
+	
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			List<String> authorities = jwt.getClaimAsStringList("authorities");
+			
+			if (authorities == null) {
+				authorities = Collections.emptyList();
+			}
+			
+			JwtGrantedAuthoritiesConverter scopesAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+					Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter.convert(jwt);
+					
+					grantedAuthorities.addAll(authorities.stream()
+							.map(SimpleGrantedAuthority::new)
+							.collect(Collectors.toList()));
+					
+					
+			return grantedAuthorities;		
+		});
+		
+		
+	return jwtAuthenticationConverter;
+	
 	}
 	
 	
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-	
-	
-	@Bean
-	public PasswordEncoder paswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
 	
 }
