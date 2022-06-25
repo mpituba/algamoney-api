28775a7242dc9ac50572916a0690da92c0b32934
Marcelo Pituba <mpituba@yahoo.com>
2022-06-25 12:02:03 -0300

    6.9.O que é Cors - Liberação global e em classe get - Não funciona com OAuth2

--- algamoney-api/src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,6 +2,10 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AlgamoneyApiApplication {
@@ -10,4 +14,15 @@ public class AlgamoneyApiApplication {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
 
+	//Liberação global de cors, não funciona com OAuth2
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8000");
+			}
+		};
+	}
+	
 }
