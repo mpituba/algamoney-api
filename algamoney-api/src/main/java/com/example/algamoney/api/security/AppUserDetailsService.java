9930f5e2a4404c92f3a6d4771da68af39e51cfab
Marcelo Pituba <mpituba@yahoo.com>
2022-06-29 17:23:47 -0300

    7.5.Nome do usuário no token JWT - correção

--- algamoney-api/src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -27,7 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuario e/ou senha incorretos"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 	
 	/* Um Set com as permissoes do usuario */
