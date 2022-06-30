d91e2e41b5eb489125998627fec5d5c58b9dea1f
Marcelo Pituba <mpituba@yahoo.com>
2022-06-30 12:35:43 -0300

    7.7.Desafio: Pesquisa de pessoa - Lista tudo, por parâmetro e paginada

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.example.algamoney.api.resource;
 
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -8,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,11 +42,12 @@ public class PessoaResource {
 	@Autowired
 	private PessoaService pessoaService;
 	
+	/* Listagem simplesde todos os registros não paginados
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and hasAuthority('SCOPE_read')")
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
-	}
+	} */
 	
 	/* Este método devolve um objeto pessoa em JSON quando esta existe
 	 * ou um código 404 not found, utilizando um map que está recebendo um objeto
@@ -57,6 +60,14 @@ public class PessoaResource {
 				.map(pessoa -> ResponseEntity.ok(pessoa))
 				.orElse(ResponseEntity.notFound().build());
 	}
+	
+	/* Método para pesquisa de pessoas */
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and hasAuthority('SCOPE_read')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
+	}
+	
 	
 	/* Este método insere uma Pessoa e retorna a uri do recurso criado com
 	 * status 201 created */	
@@ -79,7 +90,7 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA') and hasAuthority('SCOPE_write')")
 	public void remover(@PathVariable Long codigo) {
-		pessoaRepository.deleteById(codigo);
+		this.pessoaRepository.deleteById(codigo);
 	}
 	
 	/* Este método atualiza uma Pessoa com base no seu codigo*/
